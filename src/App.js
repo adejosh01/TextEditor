1fac4fe3e36c33715d95684876fee23591359400
Adeloba Joshua <adelobajoshua19@gmail.com>
2023-12-15 21:59:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "./App.css";
+import { useState } from "react";
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blackquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" }
+    ],
+    ["link", "image", "video"]
+  ]
+};
 
 function App() {
+  const [value, setValue] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3 className="heading">Welcome to the Editor</h3>
+      <div className="row">
+        <div className="editor">
+          <ReactQuill
+            theme="snow"
+            value={value}
+            onChange={setValue}
+            className="editor-input"
+            modules={modules}
+          />
+        </div>
+       
+          
+      </div>
     </div>
   );
 }
